cb075201e831fb80bc47c7f7bcd93ead6ba5cae0
sangavi <sangavi.infomover@gmail.com>
2025-08-18 20:31:30 +0530

    added changes

--- simple-todo/simple-todo/todo.py
+++ simple-todo/simple-todo/todo.py
@@ -27,10 +27,10 @@ def main():
 
     while True:
         print("\n==== To-Do App ====")
-        print("1. Add Task")
-        print("2. Show Tasks")
-        print("3. Remove Task")
-        print("4. Exit")
+        print("1. Add Task....")
+        print("2. Show Tasks....")
+        print("3. Remove Task....")
+        print("4. Exit....")
 
         choice = input("Choose an option: ")
 
